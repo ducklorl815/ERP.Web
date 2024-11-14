b638bf7db598a1bf539b8541eaa9ef10f0383c5e
ducklorl815 <75732514+ducklorl815@users.noreply.github.com>
2024-11-15 00:57:04 +0800

    初期

---
+++ ERP.Web/Controllers/SeatMapController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace ERP.Web.Controllers
+{
+    public class SeatMapController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
