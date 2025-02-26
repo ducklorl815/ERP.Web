d350d2f77ed6c0c3ab49d9deb7f22b4f24ed015e
ducklorl815 <75732514+ducklorl815@users.noreply.github.com>
2025-02-26 22:17:23 +0800

    F_新增英文試卷功能

---
+++ ERP.Web/Controllers/ExamController.cs
@@ -0,0 +1,35 @@
+﻿using ERP.Web.Service.ViewModels;
+using Microsoft.AspNetCore.Mvc;
+
+public class ExamController : Controller
+{
+    public IActionResult Index()
+    {
+        // 死資料 (未來可改為從資料庫撈取)
+        var questions = new List<ExamQuestionViewModel>
+        {
+            new ExamQuestionViewModel { Id = 1, Question = "She ____ (be) a teacher.", Answer = "is" },
+            new ExamQuestionViewModel { Id = 2, Question = "They ____ (go) to school every day.", Answer = "go" },
+            new ExamQuestionViewModel { Id = 3, Question = "The sun ____ (rise) in the east.", Answer = "rises" }
+        };
+
+        return View(questions);
+    }
+    [HttpPost]
+    public IActionResult Submit(List<string> answers)
+    {
+        var correctAnswers = new List<string> { "is", "go", "rises" };
+        int score = 0;
+
+        for (int i = 0; i < correctAnswers.Count; i++)
+        {
+            if (answers[i].Trim().ToLower() == correctAnswers[i])
+            {
+                score++;
+            }
+        }
+
+        ViewBag.Score = score;
+        return View("Result");
+    }
+}
---
+++ ERP.Web/Views/Exam/Result.cshtml.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace ERP.Web.Views.Exam
+{
+    public class ResultModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
